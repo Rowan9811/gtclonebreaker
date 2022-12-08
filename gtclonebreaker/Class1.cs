25a9182daf6ebc22e2f052a254d7342cd70fddd2
Rowan9811 <77900870+Rowan9811@users.noreply.github.com>
2022-12-08 11:20:33 -0700

    YES

---
+++ gtclonebreaker/Class1.cs
@@ -0,0 +1,103 @@
+﻿using GorillaLocomotion;
+using MelonLoader;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UniverseLib.Input;
+
+namespace gtclonebreaker
+{
+    public class Class1 : MelonMod
+    {
+        
+        private LayerMask plal;
+        
+
+
+
+        public override void OnInitializeMelon()
+        {
+            
+
+        }
+
+        public override void OnUpdate()
+        {
+            plal = Player.Instance.locomotionEnabledLayers;
+            var h = GameObject.Find("pls");
+            if (InputManager.GetKey(KeyCode.Q))
+            {
+                Player.Instance.jumpMultiplier += 1;
+                Player.Instance.maxJumpSpeed = 9999999999999999999;
+            }
+            if (InputManager.GetKey(KeyCode.E))
+            {
+                Player.Instance.jumpMultiplier += 5;
+                Player.Instance.maxJumpSpeed = 9999999999999999999;
+            }
+            if (InputManager.GetKey(KeyCode.W))
+            {
+                Player.Instance.jumpMultiplier -= 1;
+                Player.Instance.maxJumpSpeed = 9999999999999999999;
+            }
+            if (InputManager.GetKey(KeyCode.A))
+            {
+                Player.Instance.jumpMultiplier = 1.8f;
+                Player.Instance.maxJumpSpeed = 9999999999999999999;
+            }
+            if (InputManager.GetKey(KeyCode.Backspace))
+            {
+                Player.Instance.jumpMultiplier = 1.1f;
+                Player.Instance.maxJumpSpeed = 6.1f;
+            }
+            if (InputManager.GetKey(KeyCode.V))
+            {
+
+
+                Player.Instance.GetComponent<Rigidbody>().isKinematic = true;
+                Player.Instance.transform.position = Player.Instance.transform.position + Camera.current.transform.forward;
+                Player.Instance.bodyCollider.enabled = false;
+                Player.Instance.headCollider.enabled = false;
+                
+                Player.Instance.locomotionEnabledLayers = 0;
+            }
+            else
+            {
+                Player.Instance.bodyCollider.enabled = true;
+                Player.Instance.headCollider.enabled = true;
+                Player.Instance.locomotionEnabledLayers = plal;
+                Player.Instance.enabled = true;
+                Player.Instance.GetComponent<Rigidbody>().isKinematic = false;
+
+            }
+            Player.Instance.locomotionEnabledLayers = plal;
+            if (InputManager.GetKey(KeyCode.C))
+            {
+
+                GameObject.CreatePrimitive(PrimitiveType.Capsule).AddComponent<Rigidbody>().transform.position = Player.Instance.transform.position;
+
+            }
+            if (InputManager.GetKeyDown(KeyCode.R))
+            {
+
+                var plane = GameObject.CreatePrimitive(PrimitiveType.Plane).AddComponent<Rigidbody>();
+                plane.isKinematic = true;
+                plane.MovePosition(new Vector3(Player.Instance.rightHandFollower.transform.position.x, Player.Instance.rightHandFollower.transform.position.y - 0.2f, Player.Instance.rightHandFollower.transform.position.z));
+                plane.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+            if (InputManager.GetKeyDown(KeyCode.L))
+            {
+
+                var plane = GameObject.CreatePrimitive(PrimitiveType.Plane).AddComponent<Rigidbody>();
+                plane.isKinematic = true;
+                plane.MovePosition(new Vector3(Player.Instance.rightHandFollower.transform.position.x, Player.Instance.rightHandFollower.transform.position.y - 0.2f, Player.Instance.rightHandFollower.transform.position.z));
+                plane.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            }
+
+        }
+        
+    }
+
+     
+
+}
---
+++ gtclonebreaker/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// ...
+[assembly: MelonInfo(typeof(gtclonebreaker.Class1), "gorilla tag clone breaker", "0.0.4", "Rowie#6897")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("gtclonebreaker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("gtclonebreaker")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4c61a2b-69fb-4cc1-a3d6-914bc9a71c81")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
